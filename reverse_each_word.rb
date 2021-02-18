41f9ceb665596babc89b631f02fd68614e9e1b00
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-18 20:39:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def reverse_each_word (sentence)
-  variable = sentence.split
+  words = sentence.split
   binding.pry
-sentence.collect {|opposite| opposite.reverse}
+words.collect {|word| word.reverse}
 end
