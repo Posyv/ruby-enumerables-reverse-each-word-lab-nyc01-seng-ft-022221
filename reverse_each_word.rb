444867e3860aa4ac4f07b5133f0a4950fcc7f130
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-18 20:23:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word (sentence)
 sentence = ["Hello there, and how are you?"]
-sentence.collect {|opposite| opposite.reverse}
+sentence.collect.split {|opposite| opposite.reverse}
 end
